d1d1801c6e1544ec3f3bd4b96101e083e2765710
skuhlmann <samkuhlmann@gmail.com>
2023-02-24 12:04:24 -0700

    gnosis setup

--- src/targetDao.ts
+++ src/targetDao.ts
@@ -15,16 +15,16 @@ export const TARGET_DAO: {
     NFT_CLAMIERS: { [key: string]: NftClaimer };
   };
 } = {
-  "0xc035": {
-    ADDRESS: "0x3dea7058a19bf6854bb63384707139636efb99ea",
-    SAFE_ADDRESS: "0x47f327bdde3c92d82872d686bb6d7a994c22b4a5",
-    CHAIN_ID: "0x5",
+  "0xe160ba2cb286888d54cf51a8b3fc01ecce8ccb44": {
+    ADDRESS: "0xe160ba2cb286888d54cf51a8b3fc01ecce8ccb44",
+    SAFE_ADDRESS: "0xcfbda71c58d9864e196f21b8530d7a4a66a0eda0",
+    CHAIN_ID: "0x64",
     NFT_CLAMIERS: {
-      "0xaA5DC030FE06fAd5A2152d430696F66F6B4582f2": {
-        name: "Red Pils",
+      "0x29e682AF081caF314df5B9CFD1f78Eb23c2Fc456": {
+        name: "Red Pil",
         description: "A beautiful straw blond crusher of a brew",
-        nftAddress: "0xf294588CC8DBAE15230a726F0f93487eB0fcF25D",
-        shaman: "0xaA5DC030FE06fAd5A2152d430696F66F6B4582f2",
+        nftAddress: "0x018A869ee8Ba15002d0B025eeb3c776142091f97",
+        shaman: "0x29e682AF081caF314df5B9CFD1f78Eb23c2Fc456",
       },
       "0xd13b5309f3b6a54664db8c273ba314e6cd26c716": {
         name: "Spork and Sour",
@@ -33,7 +33,7 @@ export const TARGET_DAO: {
         shaman: "0xd13b5309f3b6a54664db8c273ba314e6cd26c716",
       },
       "0x63da3a1cf582e40f7b369c120e6c997c20e93b7a": {
-        name: "Green Pils",
+        name: "Green Pil",
         description: "Italian Style Kveik Pilsner",
         nftAddress: "0x6D6eF9e8eEa0C37e41e0442C2797F17c8476bEec",
         shaman: "0x63da3a1cf582e40f7b369c120e6c997c20e93b7a",
